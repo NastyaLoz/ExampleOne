61770e88718f0106ee8c272cb3767d1e7b094369
NastyaLoz <nastya.lozhenitsina@gmail.com>
2023-04-14 20:27:47 +0300

    Initial commit

---
+++ ExampleOne/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace ExampleOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // N и M - кол-во элементов в массивах A и B - string[1..100000]
+            // L - кол-во элементов в массиве результатов - int[0..100000]
+            
+            //A - массив хостов с длиной каждого элемента [2..256]
+            var A = new string[]
+            {
+                "unlock.microvirus.md",
+                "visitwar.com",
+                "visitwar.de",
+                "fruonline.co.uk",
+                "australia.open.com",
+                "credit.card.us",
+                "list.stolen.credit.card.us"
+            };
+            // B - массив запрещенных хостов с длиной каждого элемента [2..256]
+            var B = new string[]
+            {
+                "microvirus.md",
+                "visitwar.de",
+                "piratebay.co.uk",
+                "list.stolen.credit.card.us"
+            };
+            
+            var N = 10000;
+            var M = 10000;
+            var len = 256;
+            var ATest = GenerationStringArray(N, len);
+            var BTest = GenerationStringArray(M,len);
+            
+
+            Stopwatch stopwatch = Stopwatch.StartNew();// создаем и запускаем таймер
+            
+            //var result = Solution.solution(ATest, BTest);
+            var result = Solution.solution(A, B);
+            
+            stopwatch.Stop(); // смотрим сколько миллисекунд было затрачено на выполнение 
+            
+            foreach (var index in result) // выводим массив индексов свободных хостов
+                Console.Write(index);
+            
+            Console.WriteLine($"\nTime worked method 1: {stopwatch.ElapsedMilliseconds}");
+            stopwatch.Restart();
+
+            //var resultLinq = Solution.solutionOnLINQ(ATest, BTest);
+            var resultLinq = Solution.solutionOnLINQ(A, B);
+
+            stopwatch.Stop(); // смотрим сколько миллисекунд было затрачено на выполнение 
+            // resultLinq.ToList().ForEach(q=>Console.Write(q)); // выводим массив индексов свободных хостов
+            Console.WriteLine($"\nTime worked method 2: {stopwatch.ElapsedMilliseconds}");
+        }
+
+        static string[] GenerationStringArray(int num, int lenghtStr)
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.-";
+            var ends = new string[]{".com",".ru",".us",".uk",".de"};
+            var random = new Random();
+            var array = new string[num];
+            for (int i = 0; i < num; i++)
+            {
+                var list = Enumerable.Repeat(0, lenghtStr).Select(x=>chars[random.Next(chars.Length)]).ToList();
+                var str = string.Join("", list) + ends[random.Next(ends.Length)];
+                array[i] = str.Trim(new char[] {'.', '-'});
+            }
+            return array;
+        }
+        
+    }
+    
+}
---
+++ ExampleOne/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExampleOne
+{
+    public class Solution
+    {
+        public static int[] solution(string[] A, string[] B)
+        {
+            var res = new List<Tuple<int,string>>();
+            for (int i = 0; i < A.Length; i++)
+            {
+                for (int j = 0; j < B.Length; j++)
+                {
+                    if (A[i].EndsWith(B[j]))
+                        break;
+                    if (j == B.Length - 1)
+                    {
+                        res.Add(new Tuple<int, string>(i, A[i]));
+                    }
+                }
+            }
+            
+            return res.Select(q=>q.Item1).ToArray();
+        }
+        
+        public static int[] solutionOnLINQ(string[] A, string[] B)
+        {
+            var busyHosts = new List<int>();
+            var array = A.Select((v, i) => new {Index = i, Value = v}).ToList();
+            foreach (var val in B)
+            {
+                busyHosts.AddRange(array.Where(q => q.Value.EndsWith(val)).Select(q=>q.Index).ToList());
+            }
+            return array.Select(q=>q.Index).Except(busyHosts).ToArray(); // возвращаем индексы разрешенных хостов
+        }
+    }
+}
